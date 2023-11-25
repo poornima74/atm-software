4c4c1e5c4cea26d741ce42dd45d929071539749c
poornima74 <151944014+poornima74@users.noreply.github.com>
2023-11-25 12:48:31 +0530

    Add files via upload

---
+++ poornima/atm software/src/atm/Main.java
@@ -0,0 +1,10 @@
+package atm;
+
+import java.sql.SQLException;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException, SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
